79c69847f86db524dd21eaeaee96991c4963ac14
Talha Rehman <itsme.talha64@gmail.com>
2025-10-25 11:55:01 +0500

    dev2 change the welcome file text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
 
 <body
     class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
-    <h1>Hi, every one how are you!</h1>
+    <h1>I am talha rehman</h1>
 </body>
 
 </html>
